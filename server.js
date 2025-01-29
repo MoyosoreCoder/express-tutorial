de66e4efbae0d6749595af510adfb22315d328d3
Mariam <kelanimaryam1234@gmail.com>
2025-01-29 11:10:13 +0100

    using index.html with ejs extension to make post request to backend router

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 //optional, this is for router
 const userRouter = require("./routes/users.js");
 
@@ -9,13 +12,12 @@ app.use(logger);
 
 app.use("/users", userRouter);
 
-
 //beginning of codes
 app.get("/", (req, res) => {
   console.log("Hello express");
   //res.send("Hi");
   //res.download(server.js);
-  res.render(index)
+  res.render("./index");
 });
 
 function logger(req, res, next) {
