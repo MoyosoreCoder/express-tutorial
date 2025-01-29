76711d7ffbd81be358888a914d800f9824eba181
Mariam <kelanimaryam1234@gmail.com>
2025-01-29 08:52:43 +0100

    everything about route

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,34 @@
-const express = require("express");
-const app = express.Router();
+const express = require("express")
+const app = express.Router()
+
 
 router.get("/", (req, res) => {
-  console.log("Hello express");
-  res.send("Hi");
-  //res.download(server.js);
-});
-
-router.get("/users", (req, res) => {
   res.send("Hello users");
 });
-router.get("/users/new", (req, res) => {
+//always put a dynamic route up bofore the :id
+router.get("/new", (req, res) => {
   res.send("Hello new users");
-});
+})
 
-//define the path
-const userRouter = require("./routes/users");
+router.post("/", (req, res) => {
+  res.send("create user")
+})
+//dynamic route with parameter
+router.get("/:d", (req, res) => {
+  res.send(`User is ${req.param.id}`)
+})
+
+router.put("/:d", (req, res) => {
+  res.send(`update a user with ${req.param.id}`)
+})
+router.delete("/:d", (req, res) => {
+  res.send(`delete a user with ${req.param.id}`)
+})
+
+
+
 //listen to server
 app.listen(3000);
+
+
+module.exports = router
